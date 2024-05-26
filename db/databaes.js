a6e01459adc72c8f4240f5f6f006b157e14ab4a1
shivam <shivamrock887466@gmail.com>
2024-05-26 16:27:43 +0530

    git ignore added ann imlemed databse

---
+++ db/databaes.js
@@ -0,0 +1,41 @@
+const mongoose=require("mongoose");
+const { string } = require("zod");
+mongoose.connect("mongodb+srv://Shivam:Shivamrock@cluster0.baeqe26.mongodb.net/udemy");
+
+
+const userSchema=new mongoose.Schema({
+
+    username:String,
+    password:String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+
+})
+
+const adminSchema=new mongoose.Schema({
+    username:String,
+    password:String
+
+})
+
+
+const courseSchema=new mongoose.Schema({
+
+})
+
+const user=mongoose.model("user",userSchema);
+const admin=mongoose.model("admin",adminSchema);
+const courses=mongoose.model("courese",courseSchema)
+
+
+module.exports={
+    user,
+    admin,
+    courses
+}
+
+
+
+
